8d88e79f44d24ac3bec96a84983e10f13351bb55
lucia <24.damt.luciaisiegas@fundacionmontessori.com>
2024-11-20 17:05:01 +0100

    Introduccion personaje Madoe

--- fantasia_java/src/madoe/Madoe.java
+++ fantasia_java/src/madoe/Madoe.java
@@ -3,7 +3,26 @@ package madoe;
 public class Madoe {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("-- Genero: ?\n"
+				+ "-- Nombre: Madoe Falkryn\n"
+				+ "-- Especie: Feerico/Hada\n"
+				+ "-- Ocupación: Ex-Guardián de la puerta\n"
+				+ "-- Estado: Desterrado/En búsqueda y captura\n"
+				+ "-- Amistades: Animalitos del bosque\n"
+				+ "-- Comida favorita: Patas de rana especiado con polvos de hojas del árbol lloroso.\n"
+				+ "-- Hobbies: Cantar, Bailar, Emabucar niños humanos perdidos, Pescar, Beber\n"
+				+ "\n"
+				+ "-- Descripción: Personaje sin un género claro dado que puede ir cambiando la forma, mide 1,20m, mitad humano,\n"
+				+ "piel de color rosa claro, con un aspecto amigable aunque cascarrabias, con orejas puntiagudas y peludas, \n"
+				+ "un pelo largo que le llega hasta los tobillos de color blanco.\n"
+				+ "Tiene 130 años, se ha dedicado a cuidar de la puertadurante 100 de esos años pero hace 2 lo desterraron \n"
+				+ "por un malentendido. \n"
+				+ "Ojos redondos que parecen hasta de pez, cuyo color va cambiando según la luz. Al amanecer son azules como el cielo,\n"
+				+ "al atardecer morados y por la noche brillan en un color innexistente en el mundo humano. Pero debido a su naturaleza \n"
+				+ "mitad-humana sólo el ojo izquierdo sufre estos cambios. El derecho es verde.\n"
+				+ "\n"
+				+ "-- Personalidad: Travieso, egoísta, caótico, timador, amigable");
 
 	}
 
