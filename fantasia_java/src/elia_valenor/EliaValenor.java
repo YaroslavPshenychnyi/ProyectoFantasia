6b87591c564b02b81cc10cd41f03a12bf0f640b2
maria <mlozanoovejero314@gmail.com>
2024-11-20 17:38:36 +0100

    descripción de Elia Valenor

--- fantasia_java/src/elia_valenor/EliaValenor.java
+++ fantasia_java/src/elia_valenor/EliaValenor.java
@@ -3,7 +3,19 @@ package elia_valenor;
 public class EliaValenor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.print("Género: femenino\n"
+				+ "Nombre: Elia Valenor\n"
+				+ "Edad: 28 años\n"
+				+ "Especie: Humana\n"
+				+ "Ocupación: Arquera y actriz\n"
+				+ "Hobbies: Dormir, jugar a juegos de mesa\n"
+				+ "Sueños y aspiraciones: Poder vivir una vida normal fuera de la fama a la cual le arrastró su madre, "
+				+ "que quería vivir sus sueños a través de su hija\n"
+				+ "Frases/Chascarrillos: 'Dormir es para principiantes, yo entro en coma', 'A mí no me preguntes "
+				+ "sólo soy una chica'\n"
+				+ "Estado: En shock tras haber sufrido un accidente de avión y aparecer en un mundo mágico\n"
+				+ "Descripción: Mujer, complexión atlética, pelo largo de color castaño, ojos marrones\n"
+				+ "Personalidad: Confiada, insegura y observadora\n");
 
 	}
 
