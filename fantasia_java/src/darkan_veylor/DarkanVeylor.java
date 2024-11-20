4bd04740862ac03d6ecb82f2825321984be8ed5e
YaroslavPshenychnyi <24.damt.yaroslavpshenychnyi@fundacionmontessori.com>
2024-11-20 17:08:41 +0100

    Introduccion personaje Darkan Veylor

--- fantasia_java/src/darkan_veylor/DarkanVeylor.java
+++ fantasia_java/src/darkan_veylor/DarkanVeylor.java
@@ -3,8 +3,22 @@ package darkan_veylor;
 public class DarkanVeylor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Darkan Veylor");
+		System.out.println(
+				"Darkan mide aproximadamente 1.95 metros de altura.\n" + 
+				"Su cuerpo es estilizado y musculoso, con una presencia que impone respeto.\n"
+				+ "Su piel grisácea con reflejos azulados está adornada\n"
+				+ "por líneas lumínicas naturales que brillan más intensamente\n"
+				+ "según sus emociones o cuando canaliza el Éter Primordial.\n"
+				+ "Tiene el cabello largo y oscuro, casi negro, con mechones\n"
+				+ "que parecen reflejar la luz como si fueran cristalinos.\n"
+				+ "Sus ojos son de un color ámbar brillante, siempre atentos,\n"
+				+ "y su mandíbula es angulosa con una leve cicatriz que\n"
+				+ "cruza su mejilla izquierda. Su ropa combina una armadura\n"
+				+ "ligera de tonos metálicos y detalles en tonos verde esmeralda,\n"
+				+ "diseñada tanto para movilidad como para simbolizar su rango como\n"
+				+ "Arconte Errante.");
+		
 	}
 
 }
