d7661e14f52644fa027b9ec1872741615757a209
lucia <24.damt.luciaisiegas@fundacionmontessori.com>
2024-11-25 15:52:55 +0100

    1 dialogo madoe

--- fantasia_java/src/dialogo/Dialogo.java
+++ fantasia_java/src/dialogo/Dialogo.java
@@ -3,18 +3,36 @@ package dialogo;
 public class Dialogo {
 
 	public static void main(String[] args) {
-		System.out.println("Nuestra historia comienza con el sílbido del viento y la tétrica sinfonia de un bosque durante la noche.\n"
-		+ "Los bosques nunca han sido un lugar donde las razas mas civilizadas sean bienvenidas, pero a traves de éste discurría un ondulante camino que se dividía abruptamente en una encrucijada sitúada en mitad de un claro.\n" 
-	 	 +  "Y, sin embargo, ni el camino no la encrucijada de caminos eliminaban la sensación inquietante del bosque pues se percibía algo sobrenatural en el ambiente, como si ni el bosque ni el sendero fueran reales\n" 
-		+ "Si lo es o no, lo dejaremos para los eruditos pues de momento un movimiento súbito destaca sobre la música del bosque.\n" 
-		+ "De entre la arboleda sale una criatura extraña, <<mmh, un hada, extraño que una de estas criaturas acaben aquí>>, Observo con curiosidad al feerico saltar alegremente por el camino mientras tararea una de esas melodías sobrenaturales que volvería loco a un humano. Tiene la piel rosa y una melena blanca que casi tiene que esquivar para no pisarla y su apariencia cambia ligeramente a cada instante.\n" 
-		+ "En un momento parece una joven de rasgos delicado por la que cualquier rey iría a la guerra para, inmediatamente despues, poseer los rasgos duros y mirada intensa de un veterano de guerra; aunque el rasgo que mas me llama la atención son sus ojos, cuya tonalidad cambia segun el angulo por el que los miras\n" 
-		+ "Sigo al hada hasta que llega al punto central de La Encrucijada , donde se sienta bajo el poste central y saca una botella con un líquido morado brillante\n" 
-		+ "Una figura imponente camina por el camino del bosque, envuelto en una armadura que de vez en cuando resplandece bajo la luz de la lunza con tonos verdosos.\n" 
-		+ "Si bien, no es su indumentaria lo que me llama la atención, ni su melena negra sino las lineas azulads brillanes que destacan en las partes de su cuerpo no cubiertas por su ropa.\n" 
-		+ "<<Cielos, ¿como ha acabado un Arconte en La Encrucijada?, hace mucho que no me encuentro con un portador del Éter Primordial. Supongo que pronto tendré la oportunidad de preguntarle\n" 
-		+ "Vigilo como el gigante avanza hasta La Encrucijada, donde el camino se divide en 7 sendas, cada una hacia una dirección diferente del bosque y observo como su mirada se encuentran con los ojos cambiantes del hada\n" 
-		+ "Pero antes de que el arconte abra la boca para hablar, una flecha se calva en el poste peligrosamente cerca de la cara del feerico  y una joven humana de aspecto extraño sale de entre los árboles.");
+		System.out.println(
+				"Nuestra historia comienza con el sílbido del viento y la tétrica sinfonia de un bosque durante la noche.\n"
+						+ "Los bosques nunca han sido un lugar donde las razas mas civilizadas sean bienvenidas, pero a traves de éste discurría un ondulante camino \n"
+						+ "que se dividía abruptamente en una encrucijada sitúada en mitad de un claro.\n"
+						+ "Y, sin embargo, ni el camino no la encrucijada de caminos eliminaban la sensación inquietante del bosque pues se percibía algo sobrenatural en el ambiente, \n"
+						+ "como si ni el bosque ni el sendero fueran reales\n"
+						+ "Si lo es o no, lo dejaremos para los eruditos pues de momento un movimiento súbito destaca sobre la música del bosque.\n"
+						+ "De entre la arboleda sale una criatura extraña, <<mmh, un hada, extraño que una de estas criaturas acaben aquí>>, Observo con curiosidad al feerico saltar \n"
+						+ "alegremente por el camino mientras tararea una de esas melodías sobrenaturales que volvería loco a un humano. Tiene la piel rosa y una melena blanca que \n"
+						+ "casi tiene que esquivar para no pisarla y su apariencia cambia ligeramente a cada instante.\n"
+						+ "En un momento parece una joven de rasgos delicado por la que cualquier rey iría a la guerra para, inmediatamente despues, poseer los rasgos duros y mirada \n"
+						+ "intensa de un veterano de guerra; aunque el rasgo que mas me llama la atención son sus ojos, cuya tonalidad cambia segun el angulo por el que los miras\n"
+						+ "Sigo al hada hasta que llega al punto central de La Encrucijada , donde se sienta bajo el poste central y saca una botella con un líquido morado brillante\n"
+						+ "Una figura imponente camina por el camino del bosque, envuelto en una armadura que de vez en cuando resplandece bajo la luz de la lunza con tonos verdosos.\n"
+						+ "Si bien, no es su indumentaria lo que me llama la atención, ni su melena negra sino las lineas azulads brillanes que destacan en las partes de su cuerpo no \n"
+						+ "cubiertas por su ropa.\n"
+						+ "<<Cielos, ¿como ha acabado un Arconte en La Encrucijada?, hace mucho que no me encuentro con un portador del Éter Primordial. Supongo que pronto tendré \n"
+						+ "la oportunidad de preguntarle\n"
+						+ "Vigilo como el gigante avanza hasta La Encrucijada, donde el camino se divide en 7 sendas, cada una hacia una dirección diferente del bosque y observo como \n"
+						+ "su mirada se encuentran con los ojos cambiantes del hada\n"
+						+ "Pero antes de que el arconte abra la boca para hablar, una flecha se clava en el poste peligrosamente cerca de la cara del feerico y una joven humana de \n"
+						+ "aspecto extraño sale de entre los árboles.");
+		
+		System.out.println();
+		System.out.println("	-¡Que gran día para pescar! -dijo el hada alegre- ...Si no fuese por las normas del general ahora mismo me estaría divirtiendo mucho más -dijo, \n"
+				+ "esta vez con un tono más sombrío-. ¡Sólo lo aguanto mientras aclaro el malentendido con el rey! Y pensar que un día me temían todos... -¡sorprendente! \n"
+				+ "El hada ha cambiado de forma, ¡otra vez! Su rigida mandíbula se suaviza, mostrando un rostro más redondo, su cuerpo ligeramente musculado y calloso \n"
+				+ "parece perder su textura, y su mirada, antes penetrante y aterradora, ahora parece asimilarse más a la de un niño-. ¡Y así será! -la forma infantil \n"
+				+ "que se había mostrado hace un segundo ha desaparecido mostrando de nuevo esa imagen segura del hada-. Sólo espera y verás pequeño general Bloodleaf \n"
+				+ "una vez vuelva a mi puesto, nadie volverá a decir tu nombre.");
 
 	}
 
