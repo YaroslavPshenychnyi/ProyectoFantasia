3c454e8a1d312920e40a871ffcd71eafab2e95b2
smalon98@gmail.com <smalon98@gmail.com>
2024-11-26 19:55:55 +0100

    commit de la revisión de contexto tras la aparición del hada de Lucía, María te toca

--- fantasia_java/src/dialogo/Dialogo.java
+++ fantasia_java/src/dialogo/Dialogo.java
@@ -5,7 +5,7 @@ public class Dialogo {
 	public static void main(String[] args) {
 		
 		System.out.println(
-				"Nuestra historia comienza con el sílbido del viento y la tétrica sinfonia de un bosque durante la noche.\n"
+				"Nuestra historia comienza con el silbido del viento y la tétrica sinfonía de un bosque durante la noche.\n"
 						+ "Los bosques nunca han sido un lugar donde las razas mas civilizadas sean bienvenidas, pero a traves de éste discurría un ondulante camino \n"
 						+ "que se dividía abruptamente en una encrucijada sitúada en mitad de un claro.\n"
 						+ "Y, sin embargo, ni el camino no la encrucijada de caminos eliminaban la sensación inquietante del bosque pues se percibía algo sobrenatural en el ambiente, \n"
@@ -17,15 +17,7 @@ public class Dialogo {
 						+ "En un momento parece una joven de rasgos delicado por la que cualquier rey iría a la guerra para, inmediatamente despues, poseer los rasgos duros y mirada \n"
 						+ "intensa de un veterano de guerra; aunque el rasgo que mas me llama la atención son sus ojos, cuya tonalidad cambia segun el angulo por el que los miras\n"
 						+ "Sigo al hada hasta que llega al punto central de La Encrucijada , donde se sienta bajo el poste central y saca una botella con un líquido morado brillante\n"
-						+ "Una figura imponente camina por el camino del bosque, envuelto en una armadura que de vez en cuando resplandece bajo la luz de la lunza con tonos verdosos.\n"
-						+ "Si bien, no es su indumentaria lo que me llama la atención, ni su melena negra sino las lineas azulads brillanes que destacan en las partes de su cuerpo no \n"
-						+ "cubiertas por su ropa.\n"
-						+ "<<Cielos, ¿como ha acabado un Arconte en La Encrucijada?, hace mucho que no me encuentro con un portador del Éter Primordial. Supongo que pronto tendré \n"
-						+ "la oportunidad de preguntarle\n"
-						+ "Vigilo como el gigante avanza hasta La Encrucijada, donde el camino se divide en 7 sendas, cada una hacia una dirección diferente del bosque y observo como \n"
-						+ "su mirada se encuentran con los ojos cambiantes del hada\n"
-						+ "Pero antes de que el arconte abra la boca para hablar, una flecha se clava en el poste peligrosamente cerca de la cara del feerico y una joven humana de \n"
-						+ "aspecto extraño sale de entre los árboles.");
+						+ "En ese momento, me doy cuenta que la extraña criatura está hablando para sí misma y, interesado por sus palabras, me concentro en escuchar su mónologo: \n");
 		
 		System.out.println();
 		System.out.println("	-¡Que gran día para pescar! -dijo el hada alegre- ...Si no fuese por las normas del general ahora mismo me estaría divirtiendo mucho más -dijo, \n"
@@ -34,7 +26,16 @@ public class Dialogo {
 				+ "parece perder su textura, y su mirada, antes penetrante y aterradora, ahora parece asimilarse más a la de un niño-. ¡Y así será! -la forma infantil \n"
 				+ "que se había mostrado hace un segundo ha desaparecido mostrando de nuevo esa imagen segura del hada-. Sólo espera y verás pequeño general Bloodleaf \n"
 				+ "una vez vuelva a mi puesto, nadie volverá a decir tu nombre.");
-
+		System.out.println();
+		System.out.println("<<Una historia maravillosa, un hada desterrada y un plan de venganza, que ser mas interesante ha llegado a mi bosque. Una lástima que rezume maldad>>|n"
+				+ "Escucho cómo la criatura sulta varias maldciones contra ese tal general durante un buen rato hasta que algo más llama mi atención.\n"
+				+ "Por uno de los caminos que llega a la encrucijada veo llegar a una humana normal aunque su indumentaria está claramente fuera de lugar pues en lugar de los jubones\n"
+				+ " y calzones propios de los mortales mas comúnes, esta viste vaqueros destrozados y una blusa quemada, ropas propias de ese mundo triste llamado Tierra.\n"
+				+ "<<¿Cómo narices ha llegado esa humana terrestre aquí?Por su aspecto, no de una forma pacífica. Y yo que pensaba que se habían perdido todos los caminos a ese mundo gris...veremos\n"
+				+ " como reacciona al encontrarse con un hada. Tendré que estar atento e intervenir en caso de enmergencia>>\n"
+				+ "Observo como la humana se acerca cojeando hacia el hada, que hace rato ha dejado de acordarse de los ancestros del pobre general y ha vuelto a canturrear sobre pescar."
+				);
+		
 	}
 
 }
--- fantasia_java/src/narrador/Narrador.java
+++ fantasia_java/src/narrador/Narrador.java
@@ -10,11 +10,7 @@ public class Narrador {
 		+ "De entre la arboleda sale una criatura extraña, <<mmh, un hada, extraño que una de estas criaturas acaben aquí>>, Observo con curiosidad al feerico saltar alegremente por el camino mientras tararea una de esas melodías sobrenaturales que volvería loco a un humano. Tiene la piel rosa y una melena blanca que casi tiene que esquivar para no pisarla y su apariencia cambia ligeramente a cada instante.\n" 
 		+ "En un momento parece una joven de rasgos delicado por la que cualquier rey iría a la guerra para, inmediatamente despues, poseer los rasgos duros y mirada intensa de un veterano de guerra; aunque el rasgo que mas me llama la atención son sus ojos, cuya tonalidad cambia segun el angulo por el que los miras\n" 
 		+ "Sigo al hada hasta que llega al punto central de La Encrucijada , donde se sienta bajo el poste central y saca una botella con un líquido morado brillante\n" 
-		+ "Una figura imponente camina por el camino del bosque, envuelto en una armadura que de vez en cuando resplandece bajo la luz de la lunza con tonos verdosos.\n" 
-		+ "Si bien, no es su indumentaria lo que me llama la atención, ni su melena negra sino las lineas azulads brillanes que destacan en las partes de su cuerpo no cubiertas por su ropa.\n" 
-		+ "<<Cielos, ¿como ha acabado un Arconte en La Encrucijada?, hace mucho que no me encuentro con un portador del Éter Primordial. Supongo que pronto tendré la oportunidad de preguntarle\n" 
-		+ "Vigilo como el gigante avanza hasta La Encrucijada, donde el camino se divide en 7 sendas, cada una hacia una dirección diferente del bosque y observo como su mirada se encuentran con los ojos cambiantes del hada\n" 
-		+ "Pero antes de que el arconte abra la boca para hablar, una flecha se calva en el poste peligrosamente cerca de la cara del feerico  y una joven humana de aspecto extraño sale de entre los árboles.");
+		+ "En ese momento, me doy cuenta que la extraña criatura está hablando para sí misma y, interesado por sus palabras, me concentro en escuchar su mónologo: \n");
 	}
 
 }
