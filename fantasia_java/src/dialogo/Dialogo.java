2854bc14eb26c839e0e11f3e046b476dcca9a3d6
LuciaIsiegas <24.damt.luciaisiegas@fundacionmontessori.com>
2024-11-27 17:50:25 +0100

    3 dialogo Madoe

--- fantasia_java/src/dialogo/Dialogo.java
+++ fantasia_java/src/dialogo/Dialogo.java
@@ -28,24 +28,26 @@ public class Dialogo {
 				+ "una vez vuelva a mi puesto, nadie volverá a decir tu nombre.");
 		System.out.println();
 		System.out.println("<<Una historia maravillosa, un hada desterrada y un plan de venganza, que ser mas interesante ha llegado a mi bosque. Una lástima que rezume maldad>>\n"
-				+ "Escucho cómo la criatura sulta varias maldciones contra ese tal general durante un buen rato hasta que algo más llama mi atención.\n"
-				+ "Por uno de los caminos que llega a la encrucijada veo llegar a una humana normal aunque su indumentaria está claramente fuera de lugar pues en lugar de los jubones\n"
-				+ " y calzones propios de los mortales mas comúnes, esta viste unos vaqueros destrozados y una blusa quemada, ropas propias de ese mundo triste llamado Tierra.\n"
-				+ "<<¿Cómo narices ha llegado esa humana terrestre aquí? Por su aspecto, no de una forma pacífica. Y yo que pensaba que se habían perdido todos los caminos a ese mundo gris...veremos\n"
-				+ " como reacciona al encontrarse con un hada. Tendré que estar atento e intervenir en caso de enmergencia>>\n"
-				+ "Observo como la humana se acerca cojeando hacia el hada, que hace rato ha dejado de acordarse de los ancestros del pobre general y ha vuelto a canturrear sobre pescar."
+				+ "Escucho cómo la criatura suelta varias maldiciones contra ese tal general durante un buen rato hasta que algo más llama mi atención.\n"
+				+ "Por uno de los caminos que llega a la encrucijada veo llegar a una humana normal aunque su indumentaria está claramente fuera de lugar pues en lugar \n"
+				+ "de los jubones y calzones propios de los mortales mas comúnes, esta viste unos vaqueros destrozados y una blusa quemada, ropas propias de ese mundo \n"
+				+ "triste llamado Tierra.\n"
+				+ "<<¿Cómo narices ha llegado esa humana terrestre aquí? Por su aspecto, no de una forma pacífica. Y yo que pensaba que se habían perdido todos los caminos\n"
+				+ "a ese mundo gris...veremos como reacciona al encontrarse con un hada. Tendré que estar atento e intervenir en caso de enmergencia>>\n"
+				+ "Observo como la humana se acerca cojeando hacia el hada, que hace rato ha dejado de acordarse de los ancestros del pobre general y ha vuelto a canturrear\n"
+				+ " sobre pescar."
 				);
 		System.out.println();
-		System.out.println("     -Tsk, maldito palo... menudo bastón me he ido a buscar, aunque bueno, el camino tampoco es que ayude mucho. Aaah. Bueno eso y el haber sufrido "
-				+ "un accidente, haberme \ndespertado en a saber dónde y el haber perdido todas mis pertenencias, y eso que he buscado lo mejor que he podido con esta pierna maltrecha..."
-				+ " ¿Eso que eschucho es agua?\nAgua y algo más, ¿alguien está cantando? Esto no me gusta... - la joven paró en seco - ¿Quién hay ahí? - dijo lo más alto que le permitió su "
-				+ "raspada voz");
+		System.out.println("     -Tsk, maldito palo... menudo bastón me he ido a buscar, aunque bueno, el camino tampoco es que ayude mucho. Aaah. Bueno eso y el haber \n"
+				+ "sufrido un accidente, haberme despertado en a saber dónde y el haber perdido todas mis pertenencias, y eso que he buscado lo mejor que he podido con \n"
+				+ "esta pierna maltrecha... ¿Eso que eschucho es agua? Agua y algo más, ¿alguien está cantando? Esto no me gusta... - la joven paró en seco - ¿Quién hay \n"
+				+ "ahí? - dijo lo más alto que le permitió su raspada voz");
 		
 		System.out.println();
 		System.out.println("	-Lalaral- -el hada calló bruscamente tras oir una voz inesperada-. ¿Quién pregunta? -Sin levantar la mirada inclinó ligeramente la\n"
 				+ "mirada-. ");
 		System.out.println();
-		System.out.println("    -He pregunatdo yo primero - intentó sonar más valiente de lo que se sentía en aquel momento en el que el corazón parecía que se le iba a salir del pecho,\n"
+		System.out.println("    -He preguntado yo primero - intentó sonar más valiente de lo que se sentía en aquel momento en el que el corazón parecía que se le iba a salir del pecho,\n"
 				+ "la cabeza le daba vueltas y el dolor le iba a consumir. Empezó a acercarse hacia donde se encontraba el hada - Mira, sólo quiero un poco de agua y me iré, \nno quiero problemas.");
 		System.out.println();
 		System.out.println("La joven herida se acercó con cautela hacia el pequeño estanque que había mas allá del poste donde el hada estaba sentada, siempre manteniendo su mirada fija en ella, que se limitaba\n"
@@ -57,6 +59,13 @@ public class Dialogo {
 		System.out.println();
 		System.out.println("	—No esperaba encontrarme con un hada cantando y una terrestre perdida en este bosque olvidado -dijo, su voz profunda rompiendo \n"
 				+ "el aire con un peso que hacía temblar las ramas más cercanas—. Hablad, antes de que decida que no merecéis estar aquí.");
+		
+		System.out.println();
+		System.out.println("	-Jajajajaj -¡que inesperada reacción! ¿El hada está riendose? ¿Peligra su vida y su reacción es reirse a carcajadas?-. ¡¿Cómo osas \n"
+				+ "hablarme en ese tono?! Yo soy Madoe Falkryn, guardián de la puerta y habitante de este bosque.");
+		
+		System.out.println();
+		System.out.println("	-¿Falkryn..? -...-. El único ser con ese nombre fue desterrado por el rey debido a ");
 	}
 
 }
